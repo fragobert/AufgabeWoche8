953c397f0185785e7b7b07255831c7487eb67c68
Fragobert <staflerflorian@gmail.com>
2022-11-28 14:36:52 +0100

    *

--- src/NaturalNumbers.java
+++ src/NaturalNumbers.java
@@ -1,2 +1,72 @@
-package PACKAGE_NAME;public class NaturalNumbers {
+public class NaturalNumbers implements Number{
+
+    @Override
+    public void add(NaturalNumbers number) {
+        return 
+    }
+
+    @Override
+    public void subtract(Number otherNumber) {
+
+    }
+
+    @Override
+    public void multiply(Number otherNumber) {
+
+    }
+
+    @Override
+    public void divide(Number otherNumber) {
+
+    }
+
+    @Override
+    public void power(Number exponent) {
+
+    }
+
+    @Override
+    public boolean isPrime() {
+        return false;
+    }
+
+    @Override
+    public boolean isOdd() {
+        return false;
+    }
+
+    @Override
+    public boolean isEven() {
+        return false;
+    }
+
+    @Override
+    public boolean greaterThan(Number otherNumber) {
+        return false;
+    }
+
+    @Override
+    public boolean lessThan(Number otherNumber) {
+        return false;
+    }
+
+    @Override
+    public boolean equals(Number otherNumber) {
+        return false;
+    }
+
+    @Override
+    public Number toPinguNumber() {
+        return null;
+    }
+
+    @Override
+    public Number toNaturalNumber() {
+        return null;
+    }
+
+    @Override
+    public int intValue() {
+        return 0;
+    }
 }
--- src/Number.java
+++ src/Number.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public interface Number {
+public interface Number {
+
+    void add(Number otherNumber);
+    void subtract(Number otherNumber);
+    void multiply(Number otherNumber);
+    void divide(Number otherNumber);
+
+    void power(Number exponent);
+
+    boolean isPrime();
+    boolean isOdd();
+    boolean isEven();
+
+    boolean greaterThan(Number otherNumber);
+    boolean lessThan(Number otherNumber);
+    boolean equals(Number otherNumber);
+
+    Number toPinguNumber();
+    Number toNaturalNumber();
+
+    int intValue();
+    String toString();
 }
